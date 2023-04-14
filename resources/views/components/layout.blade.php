91f15dfbb3d754db72fd76f1f7464a762cd3553f
3m1n3nc3 <mygames.ng@gmail.com>
2023-04-14 12:24:08 +0100

    Add Google tag

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -26,6 +26,21 @@
     <meta name="googlebot" content="index, follow">
     {{-- Add the meta google --}}
     <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-element-bundle.min.js"></script>
+    {{-- Add the Google tag if we are online --}}
+    @if (config('app.env') == 'production')
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-4D1EEY6VGZ"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+
+            function gtag() {
+                dataLayer.push(arguments);
+            }
+            gtag('js', new Date());
+
+            gtag('config', 'G-4D1EEY6VGZ');
+        </script>
+    @endif
 
     @stack('head')
 
@@ -234,30 +249,30 @@
 
 <body class="leading-normal tracking-normal text-gray-900" style="font-family: 'Source Sans Pro', sans-serif;">
 
-    <div class="h-screen pb-14 bg-right bg-cover" style="background-image:url('bg.svg');">
+    <div class="h-screen bg-right bg-cover pb-14" style="background-image:url('bg.svg');">
         <!--Nav-->
-        <div class="w-full container mx-auto p-6">
+        <div class="container w-full p-6 mx-auto">
 
-            <div class="w-full flex items-center justify-between">
-                <a class="flex items-center text-indigo-400 no-underline hover:no-underline font-bold text-2xl lg:text-4xl"
+            <div class="flex items-center justify-between w-full">
+                <a class="flex items-center text-2xl font-bold text-indigo-400 no-underline hover:no-underline lg:text-4xl"
                     href="#">
-                    <div class="h-8 fill-current text-indigo-600 pr-2">
+                    <div class="h-8 pr-2 text-indigo-600 fill-current">
                         <img src="logo-sm.png" class="h-8 rounded-md" alt="logo" />
                     </div> LOREM TONEFLIX
                 </a>
 
-                <div class="flex w-1/2 justify-end content-center">
-                    <a class="inline-block text-blue-300 no-underline hover:text-indigo-800 hover:text-underline text-center h-10 p-2 md:h-auto md:p-4"
+                <div class="flex content-center justify-end w-1/2">
+                    <a class="inline-block h-10 p-2 text-center text-blue-300 no-underline hover:text-indigo-800 hover:text-underline md:h-auto md:p-4"
                         href="https://twitter.com/toneflix_music" target="_blank">
-                        <svg class="fill-current h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+                        <svg class="h-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
                             <path
                                 d="M30.063 7.313c-.813 1.125-1.75 2.125-2.875 2.938v.75c0 1.563-.188 3.125-.688 4.625a15.088 15.088 0 0 1-2.063 4.438c-.875 1.438-2 2.688-3.25 3.813a15.015 15.015 0 0 1-4.625 2.563c-1.813.688-3.75 1-5.75 1-3.25 0-6.188-.875-8.875-2.625.438.063.875.125 1.375.125 2.688 0 5.063-.875 7.188-2.5-1.25 0-2.375-.375-3.375-1.125s-1.688-1.688-2.063-2.875c.438.063.813.125 1.125.125.5 0 1-.063 1.5-.25-1.313-.25-2.438-.938-3.313-1.938a5.673 5.673 0 0 1-1.313-3.688v-.063c.813.438 1.688.688 2.625.688a5.228 5.228 0 0 1-1.875-2c-.5-.875-.688-1.813-.688-2.75 0-1.063.25-2.063.75-2.938 1.438 1.75 3.188 3.188 5.25 4.25s4.313 1.688 6.688 1.813a5.579 5.579 0 0 1 1.5-5.438c1.125-1.125 2.5-1.688 4.125-1.688s3.063.625 4.188 1.813a11.48 11.48 0 0 0 3.688-1.375c-.438 1.375-1.313 2.438-2.563 3.188 1.125-.125 2.188-.438 3.313-.875z">
                             </path>
                         </svg>
                     </a>
-                    <a class="inline-block text-blue-300 no-underline hover:text-indigo-800 hover:text-underline text-center h-10 p-2 md:h-auto md:p-4"
+                    <a class="inline-block h-10 p-2 text-center text-blue-300 no-underline hover:text-indigo-800 hover:text-underline md:h-auto md:p-4"
                         href="https://www.facebook.com/toneflixmusic" target="_blank">
-                        <svg class="fill-current h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+                        <svg class="h-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
                             <path d="M19 6h5V0h-5c-3.86 0-7 3.14-7 7v3H8v6h4v16h6V16h5l1-6h-6V7c0-.542.458-1 1-1z">
                             </path>
                         </svg>
@@ -268,7 +283,7 @@
 
         </div>
         <!--Main-->
-        <div class="container pt-24 md:pt-48 px-6 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+        <div class="container flex flex-col flex-wrap items-center px-6 pt-24 mx-auto md:pt-48 md:flex-row">
 
             {{ $slot }}
 
